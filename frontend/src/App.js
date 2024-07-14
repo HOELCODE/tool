c9dcc00c62d6a26a7baa880cef736a9e0badd7da
HOELCODE <hoelpro@outlook.fr>
2024-07-14 16:41:42 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './Nav.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Nav.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Nav = () => {
+    return (
+        <div>
+            headerfile
+        </div>
+    )
+}
+
+export default Nav;
